8ef703dfb4ada332b867cd17d27109d2647c3681
Roberto Silva <90939196+Robertobappe@users.noreply.github.com>
2021-09-28 20:18:26 -0300

    Add files via upload

---
+++ média.py
@@ -0,0 +1,10 @@
+pn=int(input("Digite a primeira nota:"))
+sn=int(input("Digite a segunda nota:"))
+tn=int(input("Digite a terceira nota:"))
+qn=int(input("Digite a quarta nota:"))
+
+media=((pn+sn+tn+qn)/4)
+
+print("A média aritmética é",media)
+
+
